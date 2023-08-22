0e7be58aeb1fd44232f758fb72c5a739b480f910
Jose Viloria <josevill.linuxusr@gmail.com>
2023-08-22 23:49:57 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/jmvm-at-aws-hashicorp-ambassador-program/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket_prefix = "{prefix}"
+  bucket_prefix = var.prefix
 }
